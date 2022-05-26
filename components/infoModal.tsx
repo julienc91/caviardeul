0f4d1f676f1dfb79684954bfb4105d4cf8fd892e
julien <git@julienc.io>
2022-05-26 20:37:05 +0200

    feat: better display

--- components/infoModal.tsx
+++ components/infoModal.tsx
@@ -14,15 +14,28 @@ const InfoModal: React.FC<{ open: boolean; onClose: () => void }> = ({
         <h1>Caviardeul</h1>
         <p>
           Caviardeul est un jeu reprenant le concept de{" "}
-          <a href="https://www.redactle.com/">Redactle</a> par{" "}
-          <a href="https://twitter.com/jhntrnr">John Turner</a>, mais en
-          Français.
+          <a href="https://www.redactle.com/" target="_blank" rel="noreferrer">
+            Redactle
+          </a>{" "}
+          par{" "}
+          <a
+            href="https://twitter.com/jhntrnr"
+            target="_blank"
+            rel="noreferrer"
+          >
+            John Turner
+          </a>
+          , mais en Français.
         </p>
         <p>
           Le but est de retrouver quotidiennement l&apos;article Wikipédia caché
           derrière les mots caviardés, et choisi aléatoirement parmi la liste
           des{" "}
-          <a href="https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Articles_vitaux/Niveau_4">
+          <a
+            href="https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Articles_vitaux/Niveau_4"
+            target="_blank"
+            rel="noreferrer"
+          >
             10&nbsp;000 articles vitaux de Wikipédia
           </a>{" "}
           de niveau 4.
@@ -32,7 +45,14 @@ const InfoModal: React.FC<{ open: boolean; onClose: () => void }> = ({
           Caviardeul n&apos;utilise aucune donnée personnelle. Ce jeu est
           proposé gratuitement et sans aucune publicité. Son code source est
           disponible sur{" "}
-          <a href="https://github.com/julienc91/caviardeul">GitHub</a>.
+          <a
+            href="https://github.com/julienc91/caviardeul"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          .
         </p>
       </div>
     </div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 const Navbar: React.FC<{ onShowInfoModal: () => void }> = ({
   onShowInfoModal,
@@ -7,7 +8,16 @@ const Navbar: React.FC<{ onShowInfoModal: () => void }> = ({
     <nav>
       <h1>Caviardeul</h1>
       <ul>
-        <li onClick={onShowInfoModal}>Info</li>
+        <li onClick={onShowInfoModal}>À propos</li>
+        <li>
+          <a
+            href="https://github.com/julienc91/caviardeul"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image height={30} width={30} src="/github.png" alt="GitHub" />
+          </a>
+        </li>
       </ul>
     </nav>
   );
