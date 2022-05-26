ae6d67c3890ba03d9b60927b91ea2640ca3d021d
julien <git@julienc.io>
2022-05-26 12:11:16 +0200

    feat: improve history design

--- components/history.tsx
+++ components/history.tsx
@@ -19,11 +19,15 @@ const HistoryContainer: React.FC<{
         <tbody>
           {history
             .map(([word, count], i) => {
+              const isClickable = count > 0;
               const isSelectedWord = word === selectedWord;
               return (
                 <tr
                   key={i}
-                  className={isSelectedWord ? "selected" : ""}
+                  className={
+                    (isSelectedWord ? "selected " : "") +
+                    (isClickable ? "clickable " : "")
+                  }
                   onClick={() => onSelectionChange(word)}
                 >
                   <td>{i + 1}</td>
