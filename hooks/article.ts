f6998030ac7f7324f1ef7f2077ecad8c126d1a8b
julien <git@julienc.io>
2022-05-30 21:47:08 +0200

    fix: disable auto refreshing of article data

--- hooks/article.ts
+++ hooks/article.ts
@@ -3,17 +3,23 @@ import { Article } from "../types";
 import { decode } from "../utils/encryption";
 
 export const useArticle = () => {
-  return useQuery<Article, Error>("getArticle", () => {
-    return fetch(`/api/article`)
-      .then((res) => res.json())
-      .then((res) => {
-        const { key, puzzleId, pageName, title, article } = res;
-        return {
-          puzzleId,
-          pageName: decode(pageName, key),
-          title: decode(title, key),
-          article: decode(article, key),
-        };
-      });
-  });
+  return useQuery<Article, Error>(
+    "getArticle",
+    () => {
+      return fetch(`/api/article`)
+        .then((res) => res.json())
+        .then((res) => {
+          const { key, puzzleId, pageName, title, article } = res;
+          return {
+            puzzleId,
+            pageName: decode(pageName, key),
+            title: decode(title, key),
+            article: decode(article, key),
+          };
+        });
+    },
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
 };
