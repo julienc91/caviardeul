6e2e29e4b2a425080be3f4462b0f8b84ef99b776
julien <git@julienc.io>
2022-06-30 20:48:56 +0200

    feat: burger menu for mobile

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import Image from "next/image";
 import ExternalLink from "./externalLink";
 
@@ -7,26 +7,41 @@ const Navbar: React.FC<{
   onShowScoreModal: () => void;
   onShowSettingsModal: () => void;
 }> = ({ onShowInfoModal, onShowScoreModal, onShowSettingsModal }) => {
+  const [active, setActive] = useState<boolean>(false);
+
+  const handleClick = useCallback(() => {
+    setActive(!active);
+  }, [active]);
+
   return (
     <nav>
+      <button className="hamburger" onClick={handleClick}>
+        <span className="line" />
+        <span className="line" />
+        <span className="line" />
+      </button>
       <h1>Caviardeul</h1>
-      <ul>
-        <li className="left" onClick={onShowInfoModal}>
-          À propos
-        </li>
-        <li className="left" onClick={onShowScoreModal}>
-          Scores
-        </li>
-        <li className="left" onClick={onShowSettingsModal}>
-          Options
-        </li>
-        <li className="divider" />
-        <li>
-          <ExternalLink href="https://github.com/julienc91/caviardeul">
-            <Image height={30} width={30} src="/github.png" alt="GitHub" />
-          </ExternalLink>
-        </li>
-      </ul>
+
+      <div className={"nav-links" + (active ? " active" : "")}>
+        <div className="nav-link-background" onClick={handleClick} />
+        <ul>
+          <li className="left" onClick={onShowInfoModal}>
+            À propos
+          </li>
+          <li className="left" onClick={onShowScoreModal}>
+            Scores
+          </li>
+          <li className="left" onClick={onShowSettingsModal}>
+            Options
+          </li>
+          <li className="divider" />
+          <li>
+            <ExternalLink href="https://github.com/julienc91/caviardeul">
+              <Image height={30} width={30} src="/github.png" alt="GitHub" />
+            </ExternalLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
