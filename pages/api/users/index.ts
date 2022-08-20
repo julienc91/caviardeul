031cf2c7bb2a1992bbc02c9ce09c5ce84fdaaace
julien <git@julienc.io>
2022-08-20 16:24:56 +0200

    feat: start with user model

---
+++ pages/api/users/index.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { User, Error } from "../../../types";
+import { applyCors } from "../../../utils/api";
+import prismaClient from "../../../prisma";
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<User | Error>
+) => {
+  await applyCors(req, res);
+  const { method } = req;
+  if (method !== "POST") {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).json({ error: `Method ${method} Not Allowed` });
+    return;
+  }
+
+  const user = await prismaClient.user.create({ data: {} });
+  res.status(201);
+  res.json({ id: user.id });
+};
+
+export default handler;
---
+++ prisma/migrations/20220820135232_users/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "lastSeenAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
