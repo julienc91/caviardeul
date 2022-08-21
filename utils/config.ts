226c6fa3f63f90af6d3feb0de9e930c944cf8469
julien <git@julienc.io>
2022-08-21 19:44:46 +0200

    fix: wrong domain redirect from deprecated domain

--- utils/config.ts
+++ utils/config.ts
@@ -1,12 +1,10 @@
 export const firstGameDate = new Date(2022, 4, 27);
 
-const SCHEME = process.env.NEXT_PUBLIC_SCHEME || "https";
-
 export const BASE_URL =
-  `${SCHEME}://` +
-  (process.env.NEXT_PUBLIC_BASE_DOMAIN ||
-    process.env.NEXT_PUBLIC_VERCEL_URL ||
-    "caviardeul.fr");
+  process.env.NEXT_PUBLIC_BASE_URL ||
+  (process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+    ? "https://caviardeul.fr"
+    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`);
 
 export const DEPRECATED_DOMAIN =
   process.env.NEXT_PUBLIC_DEPRECATED_DOMAIN || "caviardeul.julienc.io";
