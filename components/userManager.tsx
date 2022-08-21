c621c79421ead708426bd2d647e1843cbba4695e
julien <git@julienc.io>
2022-08-21 18:55:55 +0200

    fix: wrong url being used in vercel preview

--- components/userManager.tsx
+++ components/userManager.tsx
@@ -2,12 +2,13 @@ import React, { useCallback } from "react";
 import { UserContext } from "../utils/user";
 import { getCookie, setCookie } from "cookies-next";
 import { User } from "../types";
+import { BASE_URL } from "../utils/config";
 
 const UserManager: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const getUserId = useCallback(async () => {
     let userId = getCookie("userId");
     if (!userId) {
-      const user = (await fetch("/api/users", {
+      const user = (await fetch(`${BASE_URL}/api/users`, {
         method: "POST",
       }).then((res) => res.json())) as User;
       setCookie("userId", user.id);
@@ -19,7 +20,7 @@ const UserManager: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const saveScore = useCallback(
     async (articleId: number, nbAttempts: number, nbCorrect: number) => {
       const userId = await getUserId();
-      await fetch(`/api/users/${userId}/scores`, {
+      await fetch(`${BASE_URL}/api/users/${userId}/scores`, {
         method: "POST",
         body: JSON.stringify({ articleId, nbAttempts, nbCorrect }),
         headers: {
--- utils/config.ts
+++ utils/config.ts
@@ -1,7 +1,12 @@
 export const firstGameDate = new Date(2022, 4, 27);
 
+const SCHEME = process.env.NEXT_PUBLIC_SCHEME || "https";
+
 export const BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL ?? "https://caviardeul.fr";
+  `${SCHEME}://` +
+  (process.env.NEXT_PUBLIC_BASE_DOMAIN ||
+    process.env.NEXT_PUBLIC_VERCEL_URL ||
+    "caviardeul.fr");
 
 export const DEPRECATED_DOMAIN =
-  process.env.NEXT_PUBLIC_DEPRECATED_DOMAIN ?? "caviardeul.julienc.io";
+  process.env.NEXT_PUBLIC_DEPRECATED_DOMAIN || "caviardeul.julienc.io";
