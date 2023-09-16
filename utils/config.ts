8ecb8ca8938d3d0281a35bdfe8e20287c53ab2a1
julien <git@julienc.io>
2023-09-16 20:26:55 +0200

    chore: remove code for deprecated domain

--- utils/config.ts
+++ utils/config.ts
@@ -4,9 +4,6 @@ export const BASE_URL =
     ? "https://caviardeul.fr"
     : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`);
 
-export const DEPRECATED_DOMAIN =
-  process.env.NEXT_PUBLIC_DEPRECATED_DOMAIN || "caviardeul.julienc.io";
-
 export const COOKIE_MAX_AGE = 60 * 60 * 24 * 366; // 1 year in seconds
 
 export const LAST_SEEN_AT_UPDATE_THRESHOLD = 60 * 60; // 1h in seconds
