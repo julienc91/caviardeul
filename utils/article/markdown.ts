75570c15a0db15e5c523a3b6b86773f04c594cf2
julien <git@julienc.io>
2023-10-15 19:35:01 +0200

    refactor: use jsdom for turndownjs instead of relying on domino
    The domino library looks abandoned and had compatibility issues with the latest nextjs versions. Luckily, turndownjs accepts a JSDOM document as input and not only raw text.
    
    Workaround for:
    * https://github.com/mixmark-io/turndown/issues/439 (occurred after upgrading nextjs from 13.4 to 13.5)
    * https://github.com/mixmark-io/turndown/issues/378 (occurred after upgrading nextjs from 12.X to 13.X)

--- utils/article/markdown.ts
+++ utils/article/markdown.ts
@@ -1,9 +1,10 @@
+import { JSDOM } from "jsdom";
 import TurndownService from "turndown";
 
 import { stripArticle } from "@caviardeul/utils/article/parsing";
 
 const convertToMarkdown = (htmlContent: string): string => {
-  return new TurndownService().turndown(htmlContent);
+  return new TurndownService().turndown(JSDOM.fragment(htmlContent));
 };
 
 export const convertArticleToMarkdown = (
