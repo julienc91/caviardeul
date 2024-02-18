5848622df75dc14fdb421c3ab3e93147e453601d
julien <git@julienc.io>
2024-02-18 15:46:04 +0100

    feat: encrypt article content in payload

---
+++ backend/caviardeul/caviardeul/services/encryption.py
@@ -0,0 +1,11 @@
+from cryptography.fernet import Fernet
+
+
+def generate_encryption_key() -> str:
+    return Fernet.generate_key().decode()
+
+
+def encrypt_data(data: str, key: str) -> tuple[str, str]:
+    fernet = Fernet(key.encode())
+    encrypted_data = fernet.encrypt(data.encode())
+    return encrypted_data.decode()
