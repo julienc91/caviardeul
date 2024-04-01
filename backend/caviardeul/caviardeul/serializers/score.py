c057d8ae1cf64d5767aa4b91a975969fdf21e15e
julien <git@julienc.io>
2024-04-01 16:37:58 +0200

    feat: score endpoint

--- backend/caviardeul/caviardeul/serializers/score.py
+++ backend/caviardeul/caviardeul/serializers/score.py
@@ -1,6 +1,18 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from caviardeul.models import DailyArticleScore
+
+
+class ArticleScoreCreateSerializer(serializers.Serializer):
+    nb_attempts = serializers.IntegerField(min_value=1, required=True)
+    nb_correct = serializers.IntegerField(min_value=1, required=True)
+    article_id = serializers.CharField(required=True)
+    custom = serializers.BooleanField(required=True)
+
+    def validate(self, attrs):
+        if attrs["nb_correct"] > attrs["nb_attempts"]:
+            raise ValidationError()
 
 
 class DailyArticleScoreSerializer(serializers.Serializer):
