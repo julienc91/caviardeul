0da37c675b37b82b64fd9fcc923b6f2c8e5c9844
julien <git@julienc.io>
2024-06-08 21:25:12 +0200

    fix: login/merge workflow

--- backend/caviardeul/urls.py
+++ backend/caviardeul/urls.py
@@ -22,7 +22,7 @@ from rest_framework.routers import DefaultRouter
 from caviardeul.views.custom_article import CustomArticleViewSet
 from caviardeul.views.daily_article import DailyArticleViewSet
 from caviardeul.views.score import ScoreViewSet
-from caviardeul.views.user import UserViewSet
+from caviardeul.views.user import LoginView, UserViewSet
 
 api_router = DefaultRouter(trailing_slash=False)
 api_router.register("articles", DailyArticleViewSet)
@@ -32,5 +32,6 @@ api_router.register("users", UserViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("login", LoginView.as_view()),
     path("", include(api_router.urls)),
 ]
