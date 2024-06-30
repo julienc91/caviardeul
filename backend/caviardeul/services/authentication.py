dcf38460f14c6db865f164978da5ee5cce7a112d
julien <git@julienc.io>
2024-06-30 22:06:41 +0200

    fix: update last login

--- backend/caviardeul/services/authentication.py
+++ backend/caviardeul/services/authentication.py
@@ -1,5 +1,7 @@
+from datetime import timedelta
 from uuid import UUID
 
+from django.utils import timezone
 from rest_framework.authentication import BaseAuthentication
 
 from caviardeul.models import User
@@ -15,5 +17,10 @@ class UserIDAuthentication(BaseAuthentication):
             user = User.objects.get(id=UUID(user_id))
         except (ValueError, User.DoesNotExist):
             return None
+
+        now = timezone.now()
+        if user.last_login <= now - timedelta(hours=1):
+            user.last_login = now
+            user.save(update_fields=["last_login"])
 
         return user, None
