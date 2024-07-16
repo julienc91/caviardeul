374c9c22538ab10dfd4611f7a72e3f6eb16d84b4
julien <git@julienc.io>
2024-07-16 19:20:08 +0200

    chore: disable sentry in dev

--- backend/caviardeul/sentry.py
+++ backend/caviardeul/sentry.py
@@ -5,13 +5,18 @@ try:
 except ImportError:
     print("Sentry integration is disabled")
 else:
-    sentry_dsn = os.environ["SENTRY_DSN"]
-    sentry_environment = os.environ.get("SENTRY_ENVIRONMENT", "dev")
-    sentry_release = os.environ.get("SENTRY_RELEASE", None)
-    sentry_sdk.init(
-        dsn=sentry_dsn,
-        environment=sentry_environment,
-        release=sentry_release,
-        traces_sample_rate=1.0,
-        profiles_sample_rate=1.0,
-    )
+    sentry_dsn = os.environ.get("SENTRY_DSN")
+    if not sentry_dsn:
+        print("Sentry integration is disabled")
+    else:
+        sentry_environment = os.environ.get("SENTRY_ENVIRONMENT", "dev")
+        sentry_release = os.environ.get("SENTRY_RELEASE", None)
+        traces_sample_rate = float(os.environ.get("SENTRY_TRACES_SAMPLE_RATE", 1.0))
+        profiles_sample_rate = float(os.environ.get("SENTRY_PROFILES_SAMPLE_RATE", 1.0))
+        sentry_sdk.init(
+            dsn=sentry_dsn,
+            environment=sentry_environment,
+            release=sentry_release,
+            traces_sample_rate=traces_sample_rate,
+            profiles_sample_rate=profiles_sample_rate,
+        )
