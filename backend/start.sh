7940e427a8a4e00054b8d15a6644a6ab1c3ae938
julien <git@julienc.io>
2024-08-28 20:36:10 +0200

    fix: fix django static files not working in prod

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-python manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate --no-input
 python -m gunicorn asgi:application -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000
--- frontend/utils/config.ts
+++ frontend/utils/config.ts
@@ -1,2 +1,3 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL as string;
-export const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_BASE_URL || "https://caviardeul.fr";
+export const API_URL = `${BASE_URL}/api`;
