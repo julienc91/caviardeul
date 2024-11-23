85f3834ae954bdaa03dbbf6807befa47a86e3a42
julien <git@julienc.io>
2024-11-23 17:17:25 +0100

    fix: partially fix eslint config

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,39 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ["**/.next/*", "**/node_modules/*"],
+  },
+  ...compat.extends(
+    "next",
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "prettier",
+  ),
+  {
+    plugins: {
+      react,
+      "@typescript-eslint": typescriptEslint,
+      prettier,
+    },
+
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+];
