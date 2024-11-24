6ce316085905697b8ab71566715ff95bebb1eb61
julien <git@julienc.io>
2024-11-24 18:06:57 +0100

    feat: frontend perf improvement

---
+++ frontend/components/game/word.tsx
@@ -0,0 +1,74 @@
+import React, { useMemo } from "react";
+import { useContextSelector } from "use-context-selector";
+
+import { GameContext } from "@caviardeul/components/game/gameManager";
+import { isSelected, standardizeText } from "@caviardeul/utils/caviarding";
+
+const useStandardizedWord = (word: string) => {
+  return useMemo(() => standardizeText(word), [word]);
+};
+
+const useIsSelected = (word: string) => {
+  const standardizedWord = useStandardizedWord(word);
+  const selection = useContextSelector(
+    GameContext,
+    (context) => context.selection?.[0],
+  );
+  return !!(selection && isSelected(standardizedWord, selection));
+};
+
+const useIsRevealed = (word: string) => {
+  const standardizedWord = useStandardizedWord(word);
+  return useContextSelector(GameContext, (context) =>
+    context.revealedWords.has(standardizedWord),
+  );
+};
+
+const SelectedWrapper: React.FC<{
+  word: string;
+  children: React.ReactNode;
+}> = React.memo(({ word, children }) => {
+  const selected = useIsSelected(word);
+  if (!selected) {
+    return children;
+  }
+  return <span className="selected">{children}</span>;
+});
+
+const RevealedWord = React.memo<{ word: string }>(({ word }) => {
+  return (
+    <span className="word">
+      <SelectedWrapper word={word}>{word}</SelectedWrapper>
+    </span>
+  );
+});
+
+const CaviardedWord = React.memo<{ word: string }>(({ word }) => {
+  return (
+    <span className="word caviarded" data-word-length={word.length}>
+      {"█".repeat(word.length)}
+    </span>
+  );
+});
+
+const MaybeRevealedWord = React.memo<{ word: string }>(({ word }) => {
+  const revealed = useIsRevealed(word);
+  if (revealed) {
+    return <RevealedWord word={word} />;
+  }
+  return <CaviardedWord word={word} />;
+});
+
+const WordContainer = React.memo<{ word: string }>(({ word }) => {
+  const isOver = useContextSelector(GameContext, (context) => context.isOver);
+  if (word === undefined) {
+    return null;
+  }
+
+  if (isOver) {
+    return <RevealedWord word={word} />;
+  }
+  return <MaybeRevealedWord word={word} />;
+});
+
+export default WordContainer;
