c22a95a00dc6ff7a157bc7daefdb416fab623e58
julien <git@julienc.io>
2026-01-16 21:21:47 +0100

    feat: random validity checks on daily articles

--- backend/caviardeul/tasks/daily_article.py
+++ backend/caviardeul/tasks/daily_article.py
@@ -1,9 +1,13 @@
+from django.db.models import F, OrderBy
 from django.utils import timezone
 
 from caviardeul.broker import broker
 from caviardeul.exceptions import ArticleFetchError
 from caviardeul.models import DailyArticle
-from caviardeul.services.articles import get_article_html_from_wikipedia
+from caviardeul.services.articles import (
+    get_article_html_from_wikipedia,
+    set_article_last_checked_at,
+)
 from caviardeul.services.logging import logger
 
 
@@ -23,4 +27,23 @@ async def check_upcoming_daily_article() -> None:
     except ArticleFetchError:
         logger.exception("Error when retrieving upcoming article", exc_info=True)
     else:
+        await set_article_last_checked_at(next_article)
         logger.info("Upcoming article fetched successfully")
+
+
+@broker.task(schedule=[{"cron": "* */6 * * *"}])
+async def check_random_daily_article() -> None:
+    article = await DailyArticle.objects.order_by(
+        OrderBy(F("last_checked_at"), nulls_first=True)
+    ).afirst()
+    if not article:
+        logger.error("No article found")
+        return
+
+    try:
+        _ = await get_article_html_from_wikipedia(article.page_id)
+    except ArticleFetchError:
+        logger.exception(f"Error when retrieving article {article.id}", exc_info=True)
+    else:
+        await set_article_last_checked_at(article)
+        logger.info(f"Article {article.id} fetched successfully")
--- backend/caviardeul/tests/tasks/test_daily_article.py
+++ backend/caviardeul/tests/tasks/test_daily_article.py
@@ -1,6 +1,10 @@
+from datetime import timedelta
+
 import pytest
+from django.utils import timezone
 
 from caviardeul.tasks import check_upcoming_daily_article
+from caviardeul.tasks.daily_article import check_random_daily_article
 from caviardeul.tests.factories import DailyArticleFactory
 
 pytestmark = pytest.mark.django_db
@@ -51,3 +55,56 @@ class TestCheckUpcomingDailyArticle:
         assert "ERROR Error when retrieving upcoming article" in caplog.text, (
             caplog.text
         )
+
+
+class TestCheckRandomDailyArticle:
+    async def test_check_random_article(self, mock_wiki_api):
+        now = timezone.now()
+        articles = [
+            await DailyArticleFactory.acreate(
+                trait_past=True, last_checked_at=now - timedelta(days=3)
+            ),
+            await DailyArticleFactory.acreate(trait_current=True, last_checked_at=None),
+            await DailyArticleFactory.acreate(
+                trait_future=True, last_checked_at=now - timedelta(days=1)
+            ),
+        ]
+
+        for expected_article in [
+            articles[1],
+            articles[0],
+            articles[2],
+            articles[1],
+            articles[0],
+        ]:
+            now = timezone.now()
+            mock_wiki_api(
+                expected_article.page_id, expected_article.page_name, "article content"
+            )
+
+            await expected_article.arefresh_from_db()
+            assert (
+                expected_article.last_checked_at is None
+                or expected_article.last_checked_at < now
+            )
+
+            await check_random_daily_article.kiq()
+
+            await expected_article.arefresh_from_db()
+            assert expected_article.last_checked_at >= now
+
+    async def test_check_random_article_error(self, mock_wiki_api_error, caplog):
+        article = await DailyArticleFactory.acreate(trait_past=True)
+        mock_wiki_api_error(article.page_id)
+
+        await check_random_daily_article.kiq()
+        assert f"ERROR Error when retrieving article {article.id}" in caplog.text, (
+            caplog.text
+        )
+
+        await article.arefresh_from_db()
+        assert article.last_checked_at is None
+
+    async def test_check_random_article_no_article(self, caplog):
+        await check_random_daily_article.kiq()
+        assert "ERROR No article found" in caplog.text, caplog.text
