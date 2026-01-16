d495bb8818df90b991ff87f2c589e81c48df275d
julien <git@julienc.io>
2026-01-16 20:34:50 +0100

    feat: use async task to check upcoming article

--- backend/caviardeul/broker.py
+++ backend/caviardeul/broker.py
@@ -1,9 +1,15 @@
 import os
 
 from django.conf import settings
-from taskiq import InMemoryBroker
+from taskiq import InMemoryBroker, TaskiqScheduler
+from taskiq.schedule_sources import LabelScheduleSource
 from taskiq_redis import RedisStreamBroker
 
 broker = RedisStreamBroker(url=f"{settings.REDIS_URL}/1")
 if os.environ.get("ENVIRONMENT") == "pytest":
     broker = InMemoryBroker(await_inplace=True)
+
+scheduler = TaskiqScheduler(
+    broker=broker,
+    sources=[LabelScheduleSource(broker)],
+)
--- backend/worker.py
+++ backend/worker.py
@@ -5,6 +5,6 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "caviardeul.settings")
 django.setup()
 
-from caviardeul.broker import broker  # noqa E402
+from caviardeul.broker import broker, scheduler  # noqa E402
 
-__all__ = ["broker"]
+__all__ = ["broker", "scheduler"]
